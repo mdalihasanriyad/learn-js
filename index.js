899628362e8253aa21a6cd5eb84e666b71fcdc4f
mdalihasanriyad <ahriyad.com@gmail.com>
2026-02-14 12:33:32 +0600

    added forEach

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const learnLeason = () => {
     .then(json => displayLeason(json.data))}
 
 const loadLevelWord = (id) => {
-    const url =`https://openapi.programming-hero.com/api/word/${id}`;
+    const url =`https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)    
     .then(response => response.json())
     .then(json => displayLevelWord(json.data));
@@ -14,37 +14,37 @@ const loadLevelWord = (id) => {
 const displayLevelWord = (words) => {
     const levelWordContainer = document.getElementById("level-word-container");
     levelWordContainer.innerHTML = "";
-    for ( const word of words){
-        const cart = document.createElement("div");
-        cart.innerHTML=`
-        <div class="bg-white shadow-xl rounded-xl text-center py-10 space-x-4 ">
-           <h2 class="text-2xl font-bold ">${words.word}</h2>
-           <p class="font-semibold text-xl my-4 ">Meaning /Pronounciation</p>
-          <h3 class="font-bangla text-2xl ">"${words.meaning} / ${words.pronunciation}"</h3>
-           <div class="flex item-center  justify-between gap-6 p-4 ">
-             <button class="btn btn-primary">Previous</button>
-             <button class="btn btn-primary">Next</button>
-           </div>
-       </div>
-        `
-        levelWordContainer.append(cart);
-    }
-    // words.forEach(word => {
-    //     console.log(word);  
+    // for ( const word of words){
     //     const cart = document.createElement("div");
-    //     cart.innerHTML = `
-    //     <div class="bg-white shadow-xl rounded-xl text-center py-10 space-x-4 ">
-    //       <h2 class="text-2xl font-bold ">${word.word}</h2>
-    //       <p class="font-semibold text-xl my-4 ">Meaning /Pronounciation</p>
+    //     cart.innerHTML=`
+    //     <div class="bg-white shadow-xl rounded-xl text-center py-10 space-x-4  gap-8">
+    //        <h2 class="text-2xl font-bold ">${word.word}</h2>
+    //        <p class="font-semibold text-xl my-4 ">Meaning /Pronounciation</p>
     //       <h3 class="font-bangla text-2xl ">"${word.meaning} / ${word.pronunciation}"</h3>
-    //       <div class="flex item-center  justify-between gap-6 p-4 ">
-    //         <button class="btn btn-primary">Previous</button>
-    //         <button class="btn btn-primary">Next</button>
-    //       </div>
-    //   </div>
+    //        <div class="flex item-center  justify-between gap-6 p-4 ">
+    //          <button class="btn btn-primary">Previous</button>
+    //          <button class="btn btn-primary">Next</button>
+    //        </div>
+    //    </div>
     //     `
     //     levelWordContainer.append(cart);
-    // });
+    // }
+    words.forEach(word => {
+        console.log(word);  
+        const cart = document.createElement("div");
+        cart.innerHTML = `
+        <div class="bg-white shadow-xl rounded-xl text-center py-10 space-x-4 ">
+          <h2 class="text-2xl font-bold ">${word.word}</h2>
+          <p class="font-semibold text-xl my-4 ">Meaning /Pronounciation</p>
+          <h3 class="font-bangla text-2xl ">"${word.meaning} / ${word.pronunciation}"</h3>
+          <div class="flex item-center  justify-between gap-6 p-4 ">
+            <button class="btn btn-primary">Previous</button>
+            <button class="btn btn-primary">Next</button>
+          </div>
+      </div>
+        `
+        levelWordContainer.append(cart);
+    });
 }
 
 const displayLeason = (leasons) => {
