0c131572fc473d25525f65db8a317eada54d51a2
mdalihasanriyad <ahriyad.com@gmail.com>
2026-02-15 12:03:18 +0600

    handel error

--- index.js
+++ index.js
@@ -14,22 +14,33 @@ const loadLevelWord = (id) => {
 const displayLevelWord = (words) => {
     const levelWordContainer = document.getElementById("level-word-container");
     levelWordContainer.innerHTML = "";
-    // for ( const word of words){
-    //     const cart = document.createElement("div");
-    //     cart.innerHTML=`
-    //     <div class="bg-white shadow-xl rounded-xl text-center py-10 space-x-4  gap-8">
-    //        <h2 class="text-2xl font-bold ">${word.word}</h2>
-    //        <p class="font-semibold text-xl my-4 ">Meaning /Pronounciation</p>
-    //       <h3 class="font-bangla text-2xl ">"${word.meaning} / ${word.pronunciation}"</h3>
-    //        <div class="flex item-center  justify-between gap-6 p-4 ">
-    //          <button class="btn btn-primary">Previous</button>
-    //          <button class="btn btn-primary">Next</button>
-    //        </div>
-    //    </div>
-    //     `
-    //     levelWordContainer.append(cart);
-    // }
-   s
+
+    if(words.length === 0){
+        levelWordContainer.innerHTML = `
+        <div class="text-center col-span-3">
+        <img src="./assets/alert-error.png" alt="alart-error" class="mx-auto mb-6 w-20" />
+        <p class="text-xl font-bangla font-medium text-gray-400 py-6">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="text-3xl font-bold ">নেক্সট Lesson এ যান</h2>
+      </div>
+        `;
+        return;
+    }
+    for ( const word of words){
+        const cart = document.createElement("div");
+        cart.innerHTML=`
+        <div class="bg-white shadow-xl rounded-xl text-center py-10 space-x-4  gap-8">
+           <h2 class="text-2xl font-bold ">${word.word}</h2>
+           <p class="font-semibold text-xl my-4 ">Meaning /Pronounciation</p>
+          <h3 class="font-bangla text-2xl ">"${word.meaning} / ${word.pronunciation}"</h3>
+           <div class="flex item-center  justify-between gap-6 p-4 ">
+             <button class="btn btn-primary">Previous</button>
+             <button class="btn btn-primary">Next</button>
+           </div>
+       </div>
+        `
+        levelWordContainer.append(cart);
+    }
+   
 }
 
 const displayLeason = (leasons) => {
